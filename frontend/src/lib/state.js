8f4e6cce5d1bac475effc3ee4ed3b7bd5f609195
jasongainor <51384777+jasongainor@users.noreply.github.com>
2026-01-28 14:55:15 -0500

    Add files via upload

---
+++ frontend/src/lib/state.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Viewer State
+ */
+
+export default {
+  camera: null,
+  canvas: null,
+  controls: null,
+  light: null,
+  meshes: null,
+  plane: null,
+  renderer: null,
+  renderID: null,
+  scene: null
+};
